c6523f5b85081403975a37f6e9c577d175ebfd16
Simon Barben <simon.barben@gmail.com>
2020-05-23 13:54:33 +0200

    Task 3.5: Implemented appointment validation

--- application/src/main/java/ch/bfh/bti7081/s2020/yellow/presenter/CreateAppointmentPresenter.java
+++ application/src/main/java/ch/bfh/bti7081/s2020/yellow/presenter/CreateAppointmentPresenter.java
@@ -10,8 +10,10 @@ import org.vaadin.stefan.fullcalendar.Entry;
 import java.sql.Timestamp;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.temporal.*;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Presenter of CreateAppointmentView
@@ -22,7 +24,7 @@ public class CreateAppointmentPresenter implements CreateAppointmentView.CreateA
     private final CreateAppointmentView view;
     private final PatientRepository patientRepository;
     private final AppointmentRepository appointmentRepository;
-    private final List<Appointment> appointments;
+    private List<Appointment> appointments;
     private final Set<Long> alreadyLoadedCalenderEntries = new TreeSet<>();
 
     /**
@@ -53,9 +55,21 @@ public class CreateAppointmentPresenter implements CreateAppointmentView.CreateA
      * Method is called on save
      */
     @Override
-    public void onSave(Timestamp appointmentStart, Timestamp appointmentEnd, Patient patient) {
-        Appointment entry = new Appointment(appointmentStart, appointmentEnd, patient);
-        appointmentRepository.save(entry);
+    public void onSave(Patient patient, LocalDate date, LocalTime startTime, LocalTime endTime) {
+        // Assemble timestamps
+        Timestamp startTimeStamp = Timestamp.valueOf(date + " " + startTime.toString() + ":00");
+        Timestamp endTimeStamp = Timestamp.valueOf(date + " " + endTime.toString() + ":00");
+
+        // Save appointment
+        Appointment appointment = new Appointment(startTimeStamp, endTimeStamp, patient);
+        appointmentRepository.save(appointment);
+
+        // Load updated appointment list
+        appointments = appointmentRepository.getAll().list();
+
+        // Reset form
+        onDatePick(date);
+        view.resetForm();
     }
 
     /**
@@ -64,33 +78,74 @@ public class CreateAppointmentPresenter implements CreateAppointmentView.CreateA
      */
     @Override
     public void onDatePick(LocalDate newDate) {
-        // get start and end date of week with picked date
-        WeekFields weekFields = WeekFields.of(Locale.getDefault());
-        TemporalField temporalField = weekFields.weekOfYear();
-        LocalDate mondayOfWeek = LocalDate.now()
-                .with(IsoFields.WEEK_OF_WEEK_BASED_YEAR, newDate.get(temporalField))
-                .with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
-        LocalDate sundayOfWeek = mondayOfWeek.plusDays(6);
+        if (newDate != null) {
+            // get start and end date of week with picked date
+            WeekFields weekFields = WeekFields.of(Locale.getDefault());
+            TemporalField temporalField = weekFields.weekOfYear();
+            LocalDate mondayOfWeek = LocalDate.now()
+                    .with(IsoFields.WEEK_OF_WEEK_BASED_YEAR, newDate.get(temporalField))
+                    .with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+            LocalDate sundayOfWeek = mondayOfWeek.plusDays(6);
 
-        ArrayList<Entry> entries = new ArrayList<>();
-        // check which appointments are in selected week
-        appointments.forEach(a -> {
-            // check if the appointment is in selected week
-            if ((a.getStartTime().toLocalDateTime().toLocalDate().compareTo(mondayOfWeek)>=0) &&
-                    (a.getStartTime().toLocalDateTime().toLocalDate().compareTo(sundayOfWeek)<=0))  {
-                // check if appointment entry is already set in calendar
-                if (alreadyLoadedCalenderEntries.add(a.getId())) {
-                    // create and add calender entry
-                    Entry entry = new Entry();
-                    entry.setTitle(a.getPatient().getFullName());
-                    entry.setStart(a.getStartTime().toLocalDateTime());
-                    entry.setEnd(a.getEndTime().toLocalDateTime());
-                    entries.add(entry);
+            ArrayList<Entry> entries = new ArrayList<>();
+            // check which appointments are in selected week
+            appointments.forEach(a -> {
+                // check if the appointment is in selected week
+                if ((a.getStartTime().toLocalDateTime().toLocalDate().compareTo(mondayOfWeek) >= 0) &&
+                        (a.getStartTime().toLocalDateTime().toLocalDate().compareTo(sundayOfWeek) <= 0)) {
+                    // check if appointment entry is already set in calendar
+                    if (alreadyLoadedCalenderEntries.add(a.getId())) {
+                        // create and add calender entry
+                        Entry entry = new Entry();
+                        entry.setTitle(a.getPatient().getFullName());
+                        entry.setStart(a.getStartTime().toLocalDateTime());
+                        entry.setEnd(a.getEndTime().toLocalDateTime());
+                        entries.add(entry);
+                    }
                 }
+            });
+            view.setAppointmentsToCalendar(entries);
+        }
+    }
+
+    @Override
+    public void validateForm(Patient patient, LocalDate date, LocalTime startTime, LocalTime endTime) {
+        // Not all fields present
+        if (patient == null || date == null || startTime == null || endTime == null) {
+            view.setFormValidity(false, null);
+            return;
+        }
+        // Assemble timestamps
+        Timestamp startTimeStamp = Timestamp.valueOf(date + " " + startTime.toString() + ":00");
+        Timestamp endTimeStamp = Timestamp.valueOf(date + " " + endTime.toString() + ":00");
+
+        // Start time is not before end time
+         if (!startTimeStamp.before(endTimeStamp)) {
+            view.setFormValidity(false, "Startzeit muss vor Endzeit liegen.");
+        } else if (!isValidTimeSlot(startTimeStamp, endTimeStamp)) {
+            // Time slot not free
+            view.setFormValidity(false, "Dieser Zeitraum ist nicht frei.");
+        } else {
+            // Form valid
+            view.setFormValidity(true, null);
+        }
+    }
+
+    private boolean isValidTimeSlot(Timestamp startTime, Timestamp endTime) {
+        AtomicBoolean isValid = new AtomicBoolean(true);
+        appointments.forEach(appointment -> {
+            // Check if start time is during an existing appointment
+            boolean invalidStartTime = (appointment.getStartTime().after(startTime) || appointment.getStartTime().equals(startTime)) &&
+                    (appointment.getStartTime().before(endTime));
+
+            // Check if end time is during an existing appointment
+            boolean invalidEndTime = (appointment.getEndTime().after(startTime)) &&
+                    (appointment.getEndTime().before(endTime) || appointment.getEndTime().equals(endTime));
+            if (invalidStartTime || invalidEndTime) {
+                isValid.set(false);
             }
         });
-        view.setAppointmentsToCalendar(entries);
-
+        return isValid.get();
     }
 
     public Patient getPatientById(Long patientId) {
--- application/src/main/java/ch/bfh/bti7081/s2020/yellow/view/CreateAppointmentView.java
+++ application/src/main/java/ch/bfh/bti7081/s2020/yellow/view/CreateAppointmentView.java
@@ -6,6 +6,7 @@ import org.vaadin.stefan.fullcalendar.Entry;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 /**
@@ -29,11 +30,23 @@ public interface CreateAppointmentView {
     void setPatients(List<Patient> patients);
 
     /**
+     * Reset form
+     *
+     */
+    void resetForm();
+
+    /**
      * set appointment entries to calendar
      * @param appointmentEntries entry of appointment
      */
     void setAppointmentsToCalendar(List<Entry> appointmentEntries);
 
+    /**
+     * Set validity of form
+     * @param isValid is form valid
+     * @param errorMessage error message to display
+     */
+    void setFormValidity(boolean isValid, String errorMessage);
 
     /**
      * Interface to call to listeners
@@ -48,16 +61,27 @@ public interface CreateAppointmentView {
         /**
          * Method is called when save button is clicked
          *
-         * @param appointmentStart Timestamp
-         * @param appointmentEnd   Timestamp
-         * @param patient          Patient
+         * @param patient patient to save
+         * @param date date to save
+         * @param startTime startTime to save
+         * @param endTime endTime to save
          */
-        void onSave(Timestamp appointmentStart, Timestamp appointmentEnd, Patient patient);
+        void onSave(Patient patient, LocalDate date, LocalTime startTime, LocalTime endTime);
 
         /**
          * Method is called on date pick
          * @param newDate new picked date
          */
         void onDatePick(LocalDate newDate);
+
+        /**
+         * Method is called on any value change in form
+         *
+         * @param patient   currently selected patient
+         * @param date      currently selected date
+         * @param startTime currently selected startTime
+         * @param endTime   currently selected endTime
+         */
+        void validateForm(Patient patient, LocalDate date, LocalTime startTime, LocalTime endTime);
     }
 }
